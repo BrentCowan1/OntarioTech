c985c3fbc24a2e3445cde76bc3382f646a13a850
BrentCowan1 <70729489+BrentCowan1@users.noreply.github.com>
2022-09-18 15:22:34 -0230

    Add files via upload

---
+++ ScenarioEditor/src/components/options/WarningTextOption.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p class="warning">
+      {{ value }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class WarningTextOption extends Vue {
+  @Prop({ type: String, default: "" })
+  value!: string;
+}
+</script>
+
+<style scoped>
+.warning {
+  padding: 5px;
+  background-color: rgb(255, 245, 0);
+  color: black;
+  border-radius: 3px;
+  margin-bottom: 0;
+  /* font-weight: bold; */
+}
+</style>
